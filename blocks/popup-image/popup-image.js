11689912f4e06122611ae0fe42a4a3cb87e505c6
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-03-30 10:05:47 +0300

    Добавил функциональстость для popupImage

---
+++ blocks/popup-image/popup-image.js
@@ -0,0 +1,27 @@
+// Open popup-image
+const cards = document.querySelector('.elements__cards');
+const popupImage = document.querySelector('.popup-image');
+
+function openPopupImage(evt) {
+  const target = evt.target;
+  const image = popupImage.querySelector('.popup-image__image');
+  const title = popupImage.querySelector('.popup-image__title');
+  if (target.classList.contains('elements__image')){
+    image.src = target.src;
+    image.alt = target.alt;
+    title.textContent = target.alt;
+    popupImage.classList.add('popup-image_opened');
+  }
+}
+
+// PopupImage close
+const popupImageCloseButton = popupImage.querySelector('.popup-image__close-button');
+
+function closePopupImage(){
+  popupImage.classList.remove('popup-image_opened');
+}
+
+cards.addEventListener('click', openPopupImage);
+popupImageCloseButton.addEventListener('click', closePopupImage);
+
+
