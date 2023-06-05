45a87c71e9f1756509e0a11495ce32b27ab98502
Denis Latyshev <d.d.latyshev@yandex.ru>
2023-06-05 12:24:34 +0300

    файл Readme дополнен информацией о соисполнителе проекта

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,19 +3,18 @@ export default class Popup {
     this._selector = selector;
     this._popup = document.querySelector(this._selector);
     this._handleEscClose = this._handleEscClose.bind(this);
-    this._overlayClosePopup = this._overlayClosePopup.bind(this);
   }
 
   open() {
-    this._popup.classList.add('popup_opened');
+    document.querySelector(this._selector).classList.add('popup_opened');
     document.addEventListener('keydown', this._handleEscClose);
-    this._popup.addEventListener('click', this._overlayClosePopup);
+    document.querySelector(this._selector).addEventListener('click', (evt) => {this._overlayClosePopup(evt)});
   }
 
   close() {
-    this._popup.classList.remove('popup_opened');
+    document.querySelector(this._selector).classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose);
-    this._popup.removeEventListener('click', this._overlayClosePopup);
+    document.querySelector(this._selector).removeEventListener('click', (evt) => {this._overlayClosePopup(evt)});
   }
 
   _handleEscClose(evt) {
@@ -23,13 +22,13 @@ export default class Popup {
       this.close();
     }
   }
-
   _overlayClosePopup(evt) {
     if (evt.target.closest('.popup__container')) {
       return;
     }
     this.close();
   }
+
 
   setEventListeners() {
     const closeButton = this._popup.querySelector('.popup__close-button');
